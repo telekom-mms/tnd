9efcde7dcc361151d1b71014b77872a82fbcfb6f
hwipl <33433250+hwipl@users.noreply.github.com>
2023-12-15 15:45:31 +0100

    Update tests in package tnd
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pkg/tnd/detector_test.go
+++ pkg/tnd/detector_test.go
@@ -5,8 +5,11 @@ import (
 	"encoding/hex"
 	"errors"
 	"net"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
 // testWatcher is a watcher that implements the routes.Watcher and
@@ -45,6 +48,122 @@ func TestDetectorSetGetDialer(t *testing.T) {
 	got := tnd.GetDialer()
 	if got != want {
 		t.Errorf("got %p, want %p", got, want)
+	}
+}
+
+// TestDetectorProbe tests probe of Detector.
+func TestDetectorProbe(t *testing.T) {
+	// start test https server
+	ts := httptest.NewTLSServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {}))
+	defer ts.Close()
+
+	// create detector
+	tnd := NewDetector(NewConfig())
+
+	// test untrusted
+	tnd.SetServers(map[string]string{ts.URL: "invalid"})
+	go tnd.probe()
+
+	want := false
+	got := <-tnd.probeResults
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+
+	// test trusted
+	cert := ts.Certificate()
+	sha := sha256.Sum256(cert.Raw)
+	hash := hex.EncodeToString(sha[:])
+	tnd.SetServers(map[string]string{ts.URL: hash})
+	go tnd.probe()
+
+	want = true
+	got = <-tnd.probeResults
+	if got != want {
+		t.Errorf("got %t, want %t", got, want)
+	}
+}
+
+// TestDetectorHandleProbeRequest tests handleProbeRequest of Detector.
+func TestDetectorHandleProbeRequest(t *testing.T) {
+	// create detector
+	tnd := NewDetector(NewConfig())
+
+	// already running
+	tnd.running = true
+	tnd.handleProbeRequest()
+	if tnd.runAgain != true {
+		t.Error("run again should be true")
+	}
+
+	// not runnnig
+	tnd.running = false
+	tnd.handleProbeRequest()
+	if tnd.running != true {
+		t.Error("running should be true")
+	}
+
+	close(tnd.done)
+}
+
+// TestDetectorHandleProbeResult tests handleProbeResult of Detector.
+func TestDetectorHandleProbeResult(t *testing.T) {
+	// create detector
+	tnd := NewDetector(NewConfig())
+
+	// expire timer
+	tnd.timer = time.NewTimer(0)
+
+	// drain results channel
+	go func() {
+		for range tnd.results {
+		}
+	}()
+
+	// test not trusted
+	tnd.running = true
+	tnd.handleProbeResult(false)
+	if tnd.running != false {
+		t.Error("running should be false")
+	}
+
+	// test trusted
+	tnd.running = true
+	tnd.handleProbeResult(true)
+	if tnd.running != false {
+		t.Error("running should be false")
+	}
+
+	// test with runAgain
+	tnd.running = true
+	tnd.runAgain = true
+	tnd.handleProbeResult(false)
+	if tnd.runAgain != false {
+		t.Error("runAgain should be false")
+	}
+
+	close(tnd.results)
+}
+
+// TestDetectorHandleTimer tests handleTimer of Detector.
+func TestDetectorHandleTimer(t *testing.T) {
+	// create detector
+	tnd := NewDetector(NewConfig())
+
+	// expire timer
+	tnd.timer = time.NewTimer(0)
+
+	// test without already running
+	tnd.handleTimer()
+	if tnd.running != true {
+		t.Error("running should be true")
+	}
+
+	// test with already running
+	tnd.handleTimer()
+	if tnd.running != true {
+		t.Error("running should be true")
 	}
 }
 
